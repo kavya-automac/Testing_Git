c40107ed215109cb9baecca7c09330fb624941c9
neeraj <neeraj.automac@gmail.com>
2024-01-11 18:24:29 +0530

    0005 file

--- test_migrations/m_app/models.py
+++ test_migrations/m_app/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Role(models.Model):
     objects = models.Manager()
-    name=models.CharField(max_length=100,null=False)
+    namee=models.CharField(max_length=100,null=False)
 
     def __str__(self):  # to display the role name in admin page
         return self.name
