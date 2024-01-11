7c164f4a0b2a4221638a5bc797452c7244ef4789
kavya <setavakavya3@gmail.com>
2024-01-11 18:23:21 +0530

    new tablw 005 migrations

--- test_migrations/m_app/models.py
+++ test_migrations/m_app/models.py
@@ -15,3 +15,10 @@ class Role1(models.Model):
 
     def __str__(self):  # to display the role name in admin page
         return self.name1
+
+class Role2(models.Model):
+    objects = models.Manager()
+    name2=models.CharField(max_length=100,null=False)
+
+    def __str__(self):  # to display the role name in admin page
+        return self.name2
