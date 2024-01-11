b15fc118f075988532a4c04a8494de0ccadac02f
neeraj <neeraj.automac@gmail.com>
2024-01-11 16:05:15 +0530

    added function in views

--- test_migrations/m_app/views.py
+++ test_migrations/m_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+def my_fun():
+
+    print("adding print statement-neeraj")
+
+    print("overriding")
+
+    a=2
+    b=3
+    c=a*b
+    print('c',c,a,b)
+    return c
+print(my_fun())
